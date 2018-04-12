263a7d12850a92651bb3dd4113d305b3a1f0b224
Vijaya.Harsha <Vijaya.Harsha@techaspect.com>
2018-04-12 19:56:06 +0530

    initial commit

---
+++ Sample.py
@@ -0,0 +1,17 @@
+print('Hello World!!!')
+
+print (1 + 2)
+
+print ( 5 * 6 )
+# Here while dividing / we can get the output in float
+print ( 55 / 5 )
+#when we add one more / to / we get the output in  int
+print ( 55 // 5 )
+
+var1="Hi"
+var2="Harsha"
+var3="...!!"
+
+print(var1 + var2 + var3)
+
+print (var1 + ' ' + var2 + ' ' + var3 )
---
+++ Session 3.py
@@ -0,0 +1,18 @@
+_author_ = 'Harsha'
+
+# variable can be start by using Lower/Uppercase/Underscore  --> _var or Var or var
+
+_Name = "Dhulipala"
+Name1 = "Vijaya"
+name2 = "Harsha"
+age = 27
+
+print(_Name + ' ' + Name1 + ' ' + name2 )
+
+#    print (Name1 + age)
+#    TypeError: must be str, not int
+#    We cant concatenate a String and an Int datatypes
+
+print (Name1 + age)
+
+
