1aa5c1f51447b84dea7b339cf9d5cfd327483a6f
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-13 14:40:01 +0530

    Session 4
    For loop with an example

--- Session 4.py
+++ Session 4.py
@@ -1,3 +1,11 @@
 _author_ = "Harsha"
 
 # Session 4
+
+#  For Loop example
+
+for i in range(1,10):
+    print(i)
+
+# for loop syntax is nearly same as the bash but there is some change in  syntax
+# for i in range (min,max):
