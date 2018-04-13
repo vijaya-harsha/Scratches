9966de04e8694c2fc9079d7d150b6c0da22a2005
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-13 18:55:37 +0530

    Session 6 String formating

--- Session 6.py
+++ Session 6.py
@@ -1,9 +1,16 @@
+# Session 6
+# String Formating
 _author_ = 'Harsha'
 
-age = 24
+age = 27
 
 Name = "Vijaya Harsha"
 
 print(Name + " " + str(age) + "Years")
 
 # We use str method to convert the int to string
+
+print("My age is {0} years".format(age))
+
+# {0} is called a Replacement field and we replace by using it with using .format()
+
