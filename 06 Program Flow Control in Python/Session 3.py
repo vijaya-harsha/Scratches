308a50565437059b42f020b292e245057db21cdc
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-17 20:29:51 +0530

    Session 3

---
+++ 06 Program Flow Control in Python/Session 3.py
@@ -0,0 +1,12 @@
+_Author = 'Harsha'
+
+# Session 3
+# If condtion with String Comparison
+
+letter = input("Enter a Random Letter:")
+Name = input("Enter a Random Name:")
+
+if letter in Name:
+    print("Letter {} is there in Name {}".format(letter, Name))
+else:
+    print("Letter is not there in Name .. Lite thesko ..!! Bhayya ")
