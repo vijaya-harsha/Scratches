f49f6606aa0a77c949b1503fbd7cb17fa6662bd1
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-17 19:42:43 +0530

    Session 1 If condition examples

--- 06 Program Flow Control in Python/Session 1.py
+++ 06 Program Flow Control in Python/Session 1.py
@@ -2,15 +2,15 @@ _author = "Harsha"
 # Session 1
 # If Program Flow
 
-Name = input("Please enter your name: ")
-Age = int(input("Please enter your age ,{}: ".format(Name)))
-
-if Age >= 18:
-    print("Your are eligible for getting Driving License")
-else:
-    print(
-        "You are age is lower than 18 \nand not eligible for getting a Driving License\nPlease try after {} Years".format(
-            18 - Age))
+# Name = input("Please enter your name: ")
+# Age = int(input("Please enter your age ,{}: ".format(Name)))
+#
+# if Age >= 18:
+#     print("Your are eligible for getting Driving License")
+# else:
+#     print(
+#         "You are age is lower than 18 \nand not eligible for getting a Driving License\nPlease try after {} Years".format(
+#             18 - Age))
 
 # Convert string to int using below 'int' function
 
@@ -23,8 +23,12 @@ else:
 Name = input("Please enter your name: ")
 Age = int(input("Please enter your age ,{}: ".format(Name)))
 
-if Age > 18:
+if Age > 18 and Age < 60:
     print("{},You are eligible for getting Driving License \nPlease apply..!!".format(Name))
+elif Age > 60:
+    print("Don't Apply for Driving License , Take assistance of your Children")
+elif Age < 15:
+    print("You are too young to Drive .. !! Stay at home ")
 elif Age == 18:
     print("Congrats {}..!! \nYou are Just Eligible for Applying Driving License".format(Name))
 else:
