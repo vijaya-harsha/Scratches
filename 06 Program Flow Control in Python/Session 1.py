d24366ae6f281a970473206f14c468d9f7b8c57f
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-17 19:01:04 +0530

    Session 1 If condition examples

--- 06 Program Flow Control in Python/Session 1.py
+++ 06 Program Flow Control in Python/Session 1.py
@@ -8,10 +8,26 @@ Age = int(input("Please enter your age ,{}: ".format(Name)))
 if Age >= 18:
     print("Your are eligible for getting Driving License")
 else:
-    print("You are age is lower than 18 \nand not eligible for getting a Driving License\nPlease try after {} Years".format(18 - Age))
+    print(
+        "You are age is lower than 18 \nand not eligible for getting a Driving License\nPlease try after {} Years".format(
+            18 - Age))
 
 # Convert string to int using below 'int' function
 
 # We get below error when print Name with Age2 since Age2 is int datatype and Name is String
 
 # TypeError: must be str, not int
+
+# If -- Elif -- Else
+
+Name = input("Please enter your name: ")
+Age = int(input("Please enter your age ,{}: ".format(Name)))
+
+if Age > 18:
+    print("{},You are eligible for getting Driving License \nPlease apply..!!".format(Name))
+elif Age == 18:
+    print("Congrats {}..!! \nYou are Just Eligible for Applying Driving License".format(Name))
+else:
+    print(
+        "You are age is lower than 18 \nand not eligible for getting a Driving License\nPlease try after {} Years".format(
+            18 - Age))
