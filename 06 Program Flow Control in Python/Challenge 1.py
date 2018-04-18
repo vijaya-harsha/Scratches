4db13c555f961f312564a0dbe149ec1b341d5224
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-18 15:08:12 +0530

    Challenge 1

--- 06 Program Flow Control in Python/Challenge 1.py
+++ 06 Program Flow Control in Python/Challenge 1.py
@@ -4,3 +4,12 @@
 # over 18 and under 31).
 # If they are, welcome them to the holiday, otherwise print
 # a (polite) message refusing them entry.
+
+print("Please give below data to check eligibilty for going to holidays")
+Name = input("Enter your Name :")
+Age = int(input("Enter your Age:"))
+
+if (Age >= 18) and (Age < 31):
+    print("Welcome to Holiday ..!! {}".format(Name))
+else:
+    print("Sorry ..!! {} you are not allowed to go to Holiday".format(Name))
