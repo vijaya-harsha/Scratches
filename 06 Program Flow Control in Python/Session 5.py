9e03b7b8c6c6846a619a23f4336554fbbb39f7f6
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-20 19:46:48 +0530

    Session 5
    for Loop continue

--- 06 Program Flow Control in Python/Session 5.py
+++ 06 Program Flow Control in Python/Session 5.py
@@ -14,3 +14,31 @@ print("The Num is {}".format(newsep))
 # Output
 # The Num is 1233454554353756776867
 
+for android in ['CupCake', 'Donut', 'Eclair', 'Froyo', 'GingerBread', 'HoneyComb', 'Icecream Sandwich', 'JellyBean',
+                'Kitkat', 'Lolipop', 'Marshmallow', 'Nogut', 'Oreo']:
+    # format 1
+    # print("Android CodeNames are {}".format(android))
+    # format 2
+    print("Android CodeNames are " + android)
+
+# Output
+# Android CodeNames are CupCake
+# Android CodeNames are Donut
+# Android CodeNames are Eclair
+# Android CodeNames are Froyo
+# Android CodeNames are GingerBread
+# Android CodeNames are HoneyComb
+# Android CodeNames are Icecream Sandwich
+# Android CodeNames are JellyBean
+# Android CodeNames are Kitkat
+# Android CodeNames are Lolipop
+# Android CodeNames are Marshmallow
+# Android CodeNames are Nogut
+# Android CodeNames are Oreo
+
+# In below example we can see range (x,y,z)
+# 'z' is a step where number start with x and end with y skipping z items
+
+for i in range(0, 50, 5):
+    print("i is {}".format(i))
+
