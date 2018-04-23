915c97989e1422651943e5b7014c3432c6b14ec4
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-23 16:08:21 +0530

    Session 6
    Continue and Break

--- 06 Program Flow Control in Python/Session 6.py
+++ 06 Program Flow Control in Python/Session 6.py
@@ -4,20 +4,22 @@ _Author = 'Harsha'
 
 # Continue , Break & Else
 
-groceries_list = ["Atta", "Coffee Powder", "Biscuits", "Oats", "Chips"]
+groceries_list = ["Atta", "Coffee Powder", "Biscuits","Chips", "Oats"]
 
 for item in groceries_list:
     if item == 'Chips':
         break
     print("Buy " + item)
 
+print("\n")
+
 junk_item = ''
 for item in groceries_list:
     if item == 'Chips':
         junk_item = item
-        break
+        continue
     else:
-        print("Please bill me all items {}".format(item))
+        print("Please bill me item {}".format(item))
 
 # if junk_item:
 #     print("Don't eat junk food like {}".format(item))
