68382a136a2c01dcf1b489f73ff9e5d76f4b097c
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-23 17:57:24 +0530

    Session 7
    Augmented Assignments

--- 06 Program Flow Control in Python/Session 7.py
+++ 06 Program Flow Control in Python/Session 7.py
@@ -1,1 +1,18 @@
-_Author='Harsha'
+_Author = 'Harsha'
+
+x = 5
+
+x += 1          # x = x +1
+print(x)
+
+x -= 4          # x = x - 4
+print (x)
+
+x *= 5          # x = x * 5
+print(x)
+
+x /= 2          # x = x / 2
+print(x)
+
+x %= 5          # x = x % 5
+print(x)
