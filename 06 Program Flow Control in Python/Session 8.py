906210161ac223ced5c9db7a2396453f03081e72
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-26 20:29:06 +0530

    Session 8
    While Loops

--- 06 Program Flow Control in Python/Session 8.py
+++ 06 Program Flow Control in Python/Session 8.py
@@ -9,3 +9,16 @@ i = 0
 while i < 10:
     print("I value is {}".format(i))
     i += 1
+
+food_items= ["Dal", "Rice", "Curry", "Curd", "Chips"]
+
+choose=""
+while choose not in food_items:
+    choose = input("Enter food: ")
+    if choose == "Over":
+        print("I'm done")
+        break
+    elif choose in food_items:
+        print("Proceed with your Favorite meal")
+    else:
+        print("Item not available in the list .. !! \nRetry with Other Option")
