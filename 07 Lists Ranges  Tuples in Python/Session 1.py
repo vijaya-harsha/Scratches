803d96c9240fdeead747f5aa33ee9b520f1a1890
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-30 16:17:49 +0530

    Session 1
    Python list with Functions and Menthods

--- 07 Lists Ranges  Tuples in Python/Session 1.py
+++ 07 Lists Ranges  Tuples in Python/Session 1.py
@@ -1,1 +1,15 @@
 _Author = "Harsha"
+
+# Reference for Functions and Menthods for Python
+# https://www.tutorialspoint.com/python/python_lists.htm
+
+# Session 1
+
+# Lists
+
+groceries = ["Atta", "Jam", "Ketchup", "Pasta"]
+
+groceries.append("PaperBoat")
+
+for items in groceries:
+    print("Groceries that are needed " + items)
