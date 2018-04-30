40a4800c1c0affc5573a596d66488a7b13ac82ab
Vijaya Harsha <vijaya.harsha@techaspect.com>
2018-04-30 18:36:04 +0530

    Session 1
    Python list with Functions and Menthods

--- 07 Lists Ranges  Tuples in Python/Session 1.py
+++ 07 Lists Ranges  Tuples in Python/Session 1.py
@@ -12,9 +12,14 @@ groceries2 = ['Chocolates', 'Drink', 'Beer', 'Vodka']
 
 # We can add one item to a list using append() method
 
+# PaperBoat is appended to groceries
 groceries.append('PaperBoat')
 
 groceries3 = groceries + groceries2
+
+# All the groceries get sorted in ascending order
+groceries3.sort()
+sorted_groceries = groceries3
 
 groceries.count('Atta')
 
